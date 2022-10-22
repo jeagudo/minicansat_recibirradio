b93d4113ce866db3c2d371ce09bc1eca1999e2ba
J. Enrique Agudo <jeagudo@unex.es>
2022-10-22 16:43:32 +0200

    Añadir cuenteo de tiempo

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 radio.onReceivedString(function (receivedString) {
+    serial.writeNumber(Math.round(control.millis() / 1000))
+    serial.writeString(" ")
     serial.writeLine(receivedString)
 })
 radio.setGroup(1)
-serial.writeLine("Iniciando estación tierra")
+serial.writeLine("Iniciando estación tierra CANSAT 1.0")
 basic.forever(function () {
 	
 })
